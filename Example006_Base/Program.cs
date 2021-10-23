51c4ad34e57a8276feca0ca8a8cdaf5f8f4eb939
Mikhail Fedko <miklerst@mail.ru>
2021-10-24 01:14:36 +0300

    debug 006

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,31 @@
+﻿int a = new Random(). Next(1, 10);
+int b = new Random(). Next(1, 10);
+int c = new Random(). Next(1, 10);
+int d = new Random(). Next(1, 10);
+int e = new Random(). Next(1, 10);
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.Write("a = ");
+Console.WriteLine(a);
+
+Console.Write("b = ");
+Console.WriteLine(b);
+
+Console.Write("c = ");
+Console.WriteLine(c);
+
+Console.Write("d = ");
+Console.WriteLine(d);
+
+Console.Write("e = ");
+Console.WriteLine(e);
+
+Console.Write("max = ");
+Console.WriteLine(max);

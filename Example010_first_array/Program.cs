0fb07043296ce2414b6787d889fedba74f092bc3
Mr Fedko <miklerst@mail.ru>
2021-10-26 23:55:16 +0300

    example 010

---
+++ Example010_first_array/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 21, 42, 74, 96, 201, 22, 42, 536, 22 };
+int n = array.Length;
+int find = 22;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
